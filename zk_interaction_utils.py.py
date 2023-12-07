446bb94d6fc90bedb304ce9e17590ca52a2dd546
Michael George <lmech.ge@gmail.com>
2023-12-07 11:03:01 +0200

    Adding functionalities to enable the device and fetch the attendence data, and general code improvments like using password hashing for security and more code validation

--- zk_interaction_utils.py.py
+++ zk_interaction_utils.py.py
@@ -1,10 +1,12 @@
 from zk import ZK, const
 import logging
+import hashlib
+import re
 
 class ZKDeviceController:
-    def __init__(self, ip_address, port, timeout, password):
-        if not isinstance(ip_address, str):
-            raise ValueError("Invalid IP address")
+    def __init__(self, ip_address: str, port: int, timeout: int, password: str):
+        if not re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', ip_address):
+            raise ValueError("Invalid IP address format")
         if not isinstance(port, int):
             raise ValueError("Invalid port")
         if not isinstance(timeout, int):
@@ -13,14 +15,20 @@ class ZKDeviceController:
         self.ip_address = ip_address
         self.port = port
         self.timeout = timeout
-        self.password = password
-        self.zk = ZK(self.ip_address, self.port,self.timeout, self.password, force_udp=False, ommit_ping=False)
-
+        self.password = self.password = hashlib.sha256(password.encode()).hexdigest()
+        self.zk = None
+    def create_zk_instance(self):
+        try:
+            self.zk = ZK(self.ip_address, self.port,self.timeout, self.password, force_udp=False, ommit_ping=False)
+        except Exception as e:
+            logging.error(f"Failed to initialize ZK: {e}")
+            raise ValueError("Failed to initialize ZK")
 
     def connect_to_device(self):
         try:
             # connect to device
             self.connection = self.zk.connect()
+            self.connection.read_sizes()
             return self.connection
         except (Exception) as e:
             logging.error(f"Process terminate for device {self.ip_address}:{self.port} : %s", e)
@@ -30,27 +38,48 @@ class ZKDeviceController:
             self.connection.disable_device()
         else:
             raise ValueError("Invalid Connection")
+        
+    def enable_device(self):
+        if self.connection:
+            self.connection.enable_device()
+        else:
+            raise ValueError("Invalid Connection")
 
     # Function to disconnect from the fingerprint device
     def disconnect_from_device(self):
-         if self.connection:
+         try:
             self.connection.disconnect()
-         else:
+            self.connection = None
+         except:
             raise ValueError("Invalid Connection")
+        
+    def retrieve_attendance_data(self):
+        try:
+            if self.connection:
+                attendances = self.connection.get_attendance()
+                if len(attendances) == 0:
+                    logging.warning("No attendance data found")
+                return attendances
+            else:
+                logging.warning("Invalid Connection")
+                return None
+        except Exception as e:
+            logging.error(f"Error retrieving attendance data: {e}")
+            return None
         
 if __name__ == "__main__":
     # Instantiate DeviceConnection with your device's IP, port, timeout, and password
     # Should come from the interface
-    device = ZKDeviceController(ip_address='192.168.1.222', port=4370, timeout=5, password=0)
+    device = ZKDeviceController(ip_address='192.168.1.222', port=4370, timeout=5, password="0")
+    device.create_zk_instance()
 
     try:
         # Connect to the device
         connection = device.connect_to_device()
         print(connection)
-        device.zk.test_voice()
-        # Disable the device
         device.disable_device()
         print("Device disabled!")
+        device.enable_device()
     except ValueError as e:
         logging.error(f"Error: {e}")
     finally:
